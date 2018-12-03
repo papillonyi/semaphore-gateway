da83691c02212ba67b1fba11349601c7354175a8
Andy <cy39@live.cn>
2018-12-03 14:23:31 +0800

    init task struct

---
+++ semaphore/semaphore.go
@@ -0,0 +1,22 @@
+package semaphore
+
+type inventory struct {
+	content     string
+	inventoryId int
+}
+
+type template struct {
+	templateId int
+	inventory
+}
+
+type task struct {
+	projectId    int
+	repositoryId int
+	inventoryId  int
+	template     template
+	environment  string
+	playbook     string
+	debug        bool
+	dryRun       bool
+}
